64126b9cc08106de44d671a04e5928ecb55f303e
ahmad <aalmadani111@gmail.com>
2023-05-12 00:43:19 +0200

    add static and meadia settings

--- project/settings.py
+++ project/settings.py
@@ -117,6 +117,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+MEDIA_URL = ''
+MEDIA_ROOT = BASE_DIR/"meadia"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
