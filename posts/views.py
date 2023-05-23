5daef24bb1cb05208ce0d213430e13a73ed84fa8
ahmad <aalmadani111@gmail.com>
2023-05-23 23:48:21 +0200

    add redirect

--- posts/views.py
+++ posts/views.py
@@ -20,20 +20,23 @@ def new_post (request):
         form = PostForm(request.POST , request.FILES)
         if form.is_valid():
             form.save()
+            return redirect(/blog/)
 
     else:
         form = PostForm()
-    form = PostForm()
+    
     return render (request , 'posts/new.html', {'form':form})
 
-def edit_post (request ,psot_id):
+def edit_post (request ,post_id):
+    data = post.objects.get(id=post_id)
     if request.method == 'POST':
-        form = PostForm(request.POST , request.FILES)
+        form = PostForm(request.POST , request.FILES , instance=data)
         if form.is_valid():
             form.save()
+            return redirect(/blog/)
 
     else:
-        form = PostForm()
-    form = PostForm()
+        form = PostForm(instance=data)
+    
     return render (request , 'posts/edit.html', {'form':form})
 
